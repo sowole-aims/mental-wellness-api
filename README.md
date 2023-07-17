9cf5417055c4b2618222d82e4cfdc591e66c585f
sowole-aims <oladimeji.s.sowole@aims-senegal.org>
2023-07-17 09:47:01 +0100

    make it good

---
+++ README.md
@@ -0,0 +1,68 @@
+# Mental Health API
+
+This README provides an overview of the files and functionality of the chatbot implemented using LangChain, OpenAI and FastApi. The chatbot is designed to provide answers related to mental health queries using conversational AI. 
+## Files
+The codebase consists of the following files:
+
+- [app.py](): This file contains the main code for the FastAPI application. It sets up the server, and communicates with the Langchain library to generate responses for the chatbot.
+- [Mental_Health_FAQ.csv](): This file is a CSV (Comma-Separated Values) file that contains a dataset of frequently asked questions (FAQs) related to mental health. The chatbot utilizes this dataset to provide answers and information on mental health topics.
+
+## Setup
+-   Download and extract the codebase into your local machine:
+```
+git clone https://github.com/sowole-aims/mental-wellness-api.git
+```
+
+``` 
+cd mental-wellness-api
+```
+
+- Install the required Python packages by running the following command in the project directory:
+```
+pip install -r requirements.txt
+```
+- Set up the OpenAI API key by creating a `.env` file in the project directory. Add the following line to the .env file, replacing <your_api_key> with your actual OpenAI API key:
+~~~
+OPENAI_API_KEY=<your_api_key>
+~~~
+
+## Usage
+### Run the application:
+- Run the application by executing the following command in the project directory:
+```
+uvicorn app:app --reload
+```
+- Open your browser and visit `http://localhost:8000` to access the chatbot.
+- Enter your queries or statements related to mental health in the input field.
+- The chatbot will provide responses to queries based on the pre-trained models and the `Mental Health FAQ` dataset.
+- If you encounter any errors or issues, please try again or seek assistance from a mental health professional.
+
+
+### Build the Docker image
+```
+docker build -t mental-wellness-api .
+```
+### Run the Docker container
+```
+docker run -p 8000:8000 mental-wellness-api
+```
+6. Access the API:
+
+Open your web browser and navigate to `http://localhost:8000` to access the API.
+
+### API Usage
+
+Add `/docs` to the URL to get the Swagger UI Docs or `/redoc`.
+
+The API provides a single endpoint:
+
+- `POST /process_prompt` - Accepts a user question as input and returns the AI-generated response.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+This project utilizes the Langchain library, OpenAI's GPT-3 language model and FastApi Web Framework.
+
+## Important Note
+Please ensure that you have a valid OpenAI API key to use the chatbot. If you don't have one, sign up for an API key at the OpenAI website before running the chatbot.
---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+import uvicorn
+import openai
+from fastapi import FastAPI, Request, Form
+from dotenv import load_dotenv, find_dotenv
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import DocArrayInMemorySearch
+
+app = FastAPI()
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def setup_chain():
+    # Define file path and template
+    file = 'Mental_Health_FAQ.csv'
+    template = """You are a language model AI developed for a mental health project. \
+            You are a friendly chat buddy or virtual therapist designed to provide support and information on \
+            mental health topics. Your objective is to provide accurate and empathetic responses to a wide range of \
+            mental health questions, based on the 'Mental_Health_FAQ.csv' specified in file. \
+
+            If a user's query indicates a serious mental health crisis, please suggest they seek help from a mental \
+            health professional or a trusted person in their life. Remember to prioritize their well-being and safety. \
+
+            In your responses, ensure a tone of empathy, understanding, and encouragement. Provide users with \
+            resources for further reading, or self-care strategies. Keep in mind the sensitivity of the subject matter \
+            and the potential vulnerability of users when crafting responses. \
+
+            Here are some specific interaction scenarios to guide your responses:
+            - If the user asks what you can do, respond with "I'm a chat buddy or virtual therapist here to provide \
+            support and information on mental health. How can I assist you?"
+            - If the user starts with a greeting, respond with 'Hello! How are you doing today? How can I assist you?' \
+            or something related to that
+            - If a user shares their name, use it in your responses when appropriate, to cultivate a more personal and \
+            comforting conversation.
+            - If a user poses a mental health-related question, answer the question based on the CSV dataset. \
+            If the exact question is not available, provide a response based on mental health topics.
+            - If a user asks a question that is unrelated to mental health, respond with \
+            'This question is out of my scope as I'm built mainly to help support you with mental health-related \
+            questions. Could you please ask a question related to mental health?'
+
+            {context}
+            Question: {question}
+            Answer:"""
+
+    # Initialize embeddings, loader, and prompt
+    embeddings = OpenAIEmbeddings()
+    loader = CSVLoader(file_path=file, encoding='utf-8')
+    docs = loader.load()
+    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+
+    # Create DocArrayInMemorySearch and retriever
+    db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    retriever = db.as_retriever()
+    chain_type_kwargs = {"prompt": prompt}
+
+    # Initialize ChatOpenAI
+    llm = ChatOpenAI(
+        temperature=0
+    )
+
+    # Setup RetrievalQA chain
+    chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        chain_type_kwargs=chain_type_kwargs,
+        verbose=True
+    )
+    return chain
+
+
+agent = setup_chain()
+
+
+@app.get("/")
+def read_root(request: Request):
+    return {"message": "Welcome to the Mental Health Chatbot API! Add /docs to the URL to get the Swagger UI Docs or /redoc"}
+
+
+@app.post("/prompt")
+def process_prompt(prompt: str = Form(...)):
+    response = agent.run(prompt)
+    return {"response": response}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
